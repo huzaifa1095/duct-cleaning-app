2a2b9ba31e2cc27f68288f4c838b76c8327bfc41
Codekillrr <abdur.d2001@gmail.com>
2025-08-02 11:24:14 +0500

    added mission section

---
+++ src/components/Mission.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+
+const Mission = () => {
+  return (
+    <div className="hero bg-base-200 py-10">
+      <div className="hero-content flex-col text-center">
+        <div className="w-md">
+          <h1 className="text-5xl font-bold">Ducts & Vents</h1>
+        </div>
+        <p className="pt-4 pb-1 w-4xl">
+          From every states, we clean it straight Founded to deliver cleaner,
+          healthier air, DuctsandVents proudly serves customers across the
+          United States. Our mission is clear: to improve lives through
+          professional duct and vent cleaning. We recognize the importance of
+          indoor air quality and are committed to creating healthier living and
+          working environments.
+        </p>
+        <p className="pb-2 w-4xl">
+          As early adopters of best practices, we joined{" "}
+          <strong>NADCA (National Air Duct Cleaners Association)</strong> and
+          continue to meet and often exceed heir high industry standards. From
+          day one, we’ve used portable HEPA filtered vacuum syststrongs to
+          ensure thorough, efficient cleaning.
+        </p>
+
+        <h3 className="text-xl font-bold pt-2 pb-1 tracking-wider">
+          Improving Lives Through Cleaner Air
+        </h3>
+        <p className="pb-1 w-3xl">
+          At <strong>DuctsandVents</strong>, we specialize in both{" "}
+          <strong>residential and commercial duct cleaning</strong>, proudly
+          serving clients across the United States. While our core operations
+          are concentrated in states like{" "}
+          <strong>
+            Texas, Florida, California, Alabama, Arizona, Colorado, Connecticut,
+            Georgia, Illinois, Indiana, Massachusetts, Michigan, Minnesota,
+            Missouri, Nevada, New York, Oklahoma, Oregon, Pennsylvania, South
+            Carolina, Tennessee, Vermont, Virginia, Washington, West Virginia
+          </strong>
+          . Our services extend nationwide. You can find detailed service areas
+          in the “Markets” section of our website, powered by Google Maps.
+        </p>
+        <p className="pb-1 w-3xl">
+          With multiple offices, experienced account managers, and skilled field
+          technicians, we ensure quality and consistency in every project. Over
+          the years, we’ve provided{" "}
+          <strong>air duct cleaning and HVAC system restoration</strong> in
+          various locations throughout the U.S.
+        </p>
+        <p className="pb-1 w-3xl">
+          As proud members of{" "}
+          <strong>NADCA (National Air Duct Cleaners Association)</strong>, we
+          actively support the advancement of the duct cleaning industry. Our
+          leadership has played a key role in shaping industry standards. Our
+          President served on NADCA’s board for over a decade, including a term
+          as President. All work performed by DuctsandVents is overseen by
+          certified <strong>Air Systems Cleaning Specialists (ASCS)</strong> in
+          compliance with the <strong>NADCA ACR Standard</strong>, which defines
+          international benchmarks for HVAC inspection, cleaning, and
+          restoration.
+        </p>
+        <p className="pb-1 w-3xl">
+          Whether their a{" "}
+          <strong>
+            homeowner, hospital, small business, government agency
+          </strong>
+          , or a Fortune 500 company, DuctsandVents has earned the trust of a
+          wide range of clients. We are proud to be one of the most respected
+          names in the duct cleaning industry
+        </p>
+        <p className="pb-1 w-3xl">
+          <strong>Ready to breathe cleaner air?</strong> Contact us today at{" "}
+          <strong>1-786-952-3349</strong>. We're here to help.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Mission;
